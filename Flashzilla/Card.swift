002a42f942b165ffa5c0bb8a3ddd5fb23569e591
Paul Onawola <onawola.ire@gmail.com>
2023-10-20 21:20:50 +0100

    This is an ios Flash card app for learning. it allows users to create flashcards and use them when needed. The app is developed using swiftui. The app amkes accomodation for users with color issues or users using voice over by employing accessibility trick. The app also gives haptic feedback to users.

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Paul Onawola on 16/10/2023.
+//
+
+import Foundation
---
+++ Flashzilla/EditCards.swift
@@ -0,0 +1,18 @@
+//
+//  EditCards.swift
+//  Flashzilla
+//
+//  Created by Paul Onawola on 20/10/2023.
+//
+
+import SwiftUI
+
+struct EditCards: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCards()
+}
